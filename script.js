d486c30c3e96ed3486944bf86c27baf32cb98310
xu281 <165268443+xu281@users.noreply.github.com>
2024-03-29 16:14:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,14 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "I love you too! 😘";
+  question.innerHTML = "Huhuhuhu anh hết yêu bé rồi, anh là đồ tồi, còn không mau chọn lại";
   gif.src =
     "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
+  yesBtn.style.display = "none";
 });
 
-noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
-
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
-
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
+noBtn.addEventListener("click", () => {
+ question.innerHTML = "Yeahhhhh Bé cũng yêu anhhhhh 😘";
+  gif.src =
+    "https://i.imgur.com/RRhHW6N.jpeg";
 });
